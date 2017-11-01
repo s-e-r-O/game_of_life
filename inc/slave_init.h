1432715b43349cd4ab83daa799b81c5518e7d57b
Sergio Oporto Tejerina <ser.op.te@gmail.com>
2017-11-01 13:57:20 -0400

    modified way of getting neighbours ports

--- inc/slave_init.h
+++ inc/slave_init.h
@@ -3,7 +3,8 @@
 
 #define SERVER 0
 #define CLIENT 1
-
+int max(int a, int b);
+int min(int a, int b);
 int find_neighbour_ports(int id, int dim, int portnum, int neighbours_port[]);
 
 int find_initial_type(int id, int dim);
